00d82b7767ef030aa0bc7b20631d43a8c911e3c2
Niavand <mayhie50@gmail.com>
2026-05-29 12:47:16 +0330

    خوشگلاسیون

--- Base/Base.Application.Contracts/DTOs/UserDTO.cs
+++ Base/Base.Application.Contracts/DTOs/UserDTO.cs
@@ -3,12 +3,12 @@
     public class UserDTO
     {
         public long Id { get; set; }
-        public string UserName { get; set; } = string.Empty;
-        public string Role { get; set; } = string.Empty;
+        public string UserName { get; set; }
+        public string Role { get; set; }
         public string Email { get; set; }
         public string AccessToken { get; set; }
         public DateTime AccessTokenExpirtTime { get; set; }
-        public string RefreshToken { get; set; } = string.Empty;
+        public string RefreshToken { get; set; }
         public DateTime RefreshTokenExpiryTime { get; set; }
         public bool IsProfileComplete { get; set; }
 
